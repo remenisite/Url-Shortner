fed81833b8fbcb5e65f7ef03a83ba4c396207b58
antor94 <antorremeni@gmail.com>
2026-01-11 19:12:46 +0600

    change

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const dbConfig = require("./dbConfig");
-const { route } = require("./routes");
-const app = express();
 require("dotenv").config();
+const  route  = require("./routes");
+const app = express();
 
 app.use(express.json());
 
 dbConfig()
 app.use(route)
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+
 
 app.listen(8000, () => {
   console.log(`server ok`);
