f86ca1040ec0260ec4d2f07de19223dd881f388e
antor94 <antorremeni@gmail.com>
2026-01-14 13:44:31 +0600

    change

--- controllers/ShortController.js
+++ controllers/ShortController.js
@@ -3,28 +3,28 @@ const generateRandmonStr = require("../utils/randomStr");
 const { isvalidUrl } = require("../utils/validation");
 
 const createShortUrl = async (req, res) => {
-try {
-    
-      const { urlLong } = req.body;
-  if (!urlLong) return res.status(400).send({ message: "give url" });
+  try {
+    const { urlLong } = req.body;
+    if (!urlLong) return res.status(400).send({ message: "give url" });
 
-  if (!isvalidUrl(urlLong))
-    return res.status(400).send({ message: " give valid url" });
+    if (!isvalidUrl(urlLong))
+      return res.status(400).send({ message: " give valid url" });
 
-  const urlShort = generateRandmonStr();
+    const urlShort = generateRandmonStr();
 
-  const urlData = urlSchema({
-    urlLong,
-    urlShort,
-  });
-  urlData.save();
+    const urlData = urlSchema({
+      urlLong,
+      urlShort,
+    });
+    urlData.save();
 
-  res.status(201).send(urlData);
-} catch (error) {
-
-     res.status(500).send({message: "Internal server error"});
-    
-}
+    res.status(201).send({
+      urlLong: urlData.urlLong,
+      urlShort: urlData.urlShort,
+    });
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
 };
 
 module.exports = createShortUrl;
--- utils/randomStr.js
+++ utils/randomStr.js
@@ -2,14 +2,13 @@ const generateRandmonStr = (length = 5) => {
   const cherecters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 
-    let randomStr = "";
+  let randomStr = "";
   for (let i = 0; i < length; i++) {
     const randomNum = Math.floor(Math.random() * cherecters.length);
 
     randomStr += cherecters[randomNum];
-
   }
-    return randomStr;
+  return randomStr;
 };
 
 module.exports = generateRandmonStr;
