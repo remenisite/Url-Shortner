7616692c4809c5dec1cad31b99716f59ae34700e
antor94 <antorremeni@gmail.com>
2026-01-25 02:38:11 +0600

    change

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { signup, signin } = require("../controllers/authController");
+const { signup, signin, getProfile } = require("../controllers/authController");
+const { authMiddle } = require("../middleware/authmiddlewere");
 
 const routes = express.Router();
 routes.post('/signup', signup)
 routes.post("/signin", signin );
+routes.get("/getProfile" , authMiddle, getProfile)
 
 module.exports = routes;
