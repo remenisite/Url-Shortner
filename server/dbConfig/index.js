fc6edd98dcaad6bd91b918d109e0e0e03603c047
antor94 <antorremeni@gmail.com>
2026-01-27 17:39:18 +0600

    change env file

--- server/dbConfig/index.js
+++ server/dbConfig/index.js
@@ -1,17 +1,9 @@
 const mongoose = require("mongoose");
 
 const dbConfig = () => {
-    return mongoose
-      .connect(process.env.DB_STRING)
-      .then(() => console.log("database ok"));
+  return mongoose
+    .connect(process.env.DB_STRING)
+    .then(() => console.log("database ok"));
 };
 
-module.exports = dbConfig
-
-
-
-
-
-
-
-                    
+module.exports = dbConfig;
